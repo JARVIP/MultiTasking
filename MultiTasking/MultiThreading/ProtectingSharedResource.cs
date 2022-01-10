269087b95996625da018ee41bccffec71b7a1f86
oleg gugunava <oleggugunava@gmail.com>
2022-01-10 17:13:37 +0400

    ProtectingSharedResource

---
+++ MultiTasking/MultiThreading/ProtectingSharedResource.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiTasking.MultiThreading
+{
+    internal class ProtectingSharedResource
+    {
+        private int _count = 0;
+        private object _lockObject = new object();
+
+        public int Count { get { return _count; } }
+        public ProtectingSharedResource(int count)
+        {
+            _count = count;
+        }
+
+        /// <summary>
+        /// Lock
+        /// </summary>
+        internal void IncrementCount()
+        {
+            for (int i = 1; i <= 1000000; i++)
+            {
+                lock (_lockObject)
+                {
+                    _count++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Monitor
+        /// </summary>
+        internal void PrintNumbers()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Trying to enter into the critical section");
+            Monitor.Enter(_lockObject);
+            try
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " Entered into the critical section");
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write(i + ",");
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(_lockObject);
+                Console.WriteLine(Thread.CurrentThread.Name + " Exit from critical section");
+            }
+        }
+    }
+}
--- MultiTasking/Program.cs
+++ MultiTasking/Program.cs
@@ -19,18 +19,50 @@ namespace MultiTasking
             //T1.Start();
             #endregion
 
-
             #region RetrieveDataFromThreadFunction
 
-            ResultCallbackDelegate resultCallbackDelegate = new ResultCallbackDelegate(ResultCallBackMethod);
+            //ResultCallbackDelegate resultCallbackDelegate = new ResultCallbackDelegate(ResultCallBackMethod);
 
-            int number = 10;
-            RetrieveDataFromThreadFunction obj = new RetrieveDataFromThreadFunction(number, resultCallbackDelegate);
+            //int number = 10;
+            //RetrieveDataFromThreadFunction obj = new RetrieveDataFromThreadFunction(number, resultCallbackDelegate);
 
-            Thread t1 = new Thread(new ThreadStart(obj.CalculateSum));
+            //Thread t1 = new Thread(new ThreadStart(obj.CalculateSum));
 
-            t1.Start();
-            Console.Read();
+            //t1.Start();
+            //Console.Read();
+            #endregion
+
+            #region ProtectingSharedResource
+
+            //lock
+            ProtectingSharedResource obj = new ProtectingSharedResource(0);
+            //Thread t1 = new Thread(obj.IncrementCount);
+            //Thread t2 = new Thread(obj.IncrementCount);
+            //Thread t3 = new Thread(obj.IncrementCount);
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+            //t1.Join();
+            //t2.Join();
+            //t3.Join();
+            //Console.WriteLine(obj.Count);
+            //Console.Read();
+
+            //monitor
+
+            Thread[] Threads = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Threads[i] = new Thread(obj.PrintNumbers);
+                Threads[i].Name = "Child Thread " + i;
+            }
+            foreach (Thread t in Threads)
+            {
+                t.Start();
+            }
+            Console.ReadLine();
+
+
             #endregion
         }
 
